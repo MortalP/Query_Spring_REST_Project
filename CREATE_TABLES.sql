bf3265301895abd72384424b6a4a1a9a1a74654c
Sami Oksanen <sami.oksanen95@gmail.com>
2017-04-12 14:52:50 +0300

    database 0.1
    nämä testi setit projektin database-kansioon

---
+++ CREATE_TABLES.sql
@@ -0,0 +1,17 @@
+create table Kysymys(
+    id int auto_increment not null,
+    kysymys varchar(300) not null,
+    primary key (id));
+    
+create table Vastaus(
+    id int auto_increment not null,
+    vastaus varchar(300) not null,
+    kysymys_id int,
+    primary key (id),
+    foreign key (kysymys_id) REFERENCES Kysymys(id));
+    
+create table Admin(
+    kayttajatunnus varchar(50) not null,
+    salasana varchar(300) not null,
+    primary key (kayttajatunnus));
+    
---
+++ INSERT_TEST_DATA.sql
@@ -0,0 +1,14 @@
+INSERT INTO Kysymys (kysymys)
+VALUES 
+    ("Missä on jatkot?"),
+    ("Miten menee?"),
+    ("Maistuisiko olut?");
+    
+INSERT INTO Vastaus (vastaus, kysymys_id)
+VALUES 
+    ("Hyvin menee", 2),
+    ("Kyllä, kunhan ei ole Koff", 3);
+    
+INSERT INTO Admin (kayttajatunnus, salasana)
+VALUES 
+    ("devaaja", "salainen");
