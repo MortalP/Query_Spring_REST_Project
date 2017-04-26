a0fba801cef5c160820365621e9f6d17f194b4c6
jiminurmi <jimi.nurmi94@gmail.com>
2017-04-26 15:15:02 +0300

    test folder added

---
+++ src/test/java/fi/bfk/kysely/dao/KysymysDaoTest.java
@@ -0,0 +1,14 @@
+package fi.bfk.kysely.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KysymysDaoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
