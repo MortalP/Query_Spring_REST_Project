dd199cda3e6e6f3c232574f02ca334e7c733cfc5
jiminurmi <jimi.nurmi94@gmail.com>
2017-05-10 16:26:01 +0300

    Insert into muutoksia

--- database/insert_test_data_mysql.sql
+++ database/insert_test_data_mysql.sql
@@ -3,16 +3,28 @@ VALUES ("Mielipidekysely");
 
 INSERT INTO Kysymys (kysymys, tyyppi, description,kysely_id)
 VALUES 
-    ("Miss‰ on jatkot?", "Teksti", "Testitestitesti", 1 ),
+    ("Miss√§ on jatkot?", "Teksti", "Testitestitesti", 1 ),
     ("Miten menee?", "Teksti", "Kerrontaa", 1),
     ("Maistuisiko olut?", "Monivalinta", "Pilipalijuttuu", 1);
     
 INSERT INTO Vastaus (vastaus, kysymys_id)
 VALUES 
     ("Hyvin menee", 2),
-    ("Kyll‰, kunhan ei ole Koff", 3);
+    ("Kyll√§, kunhan ei ole Koff", 3);
     
-INSERT INTO Admin (kayttajatunnus, salasana)
+    
+    
+INSERT INTO webuser2
 VALUES 
-    ("devaaja", "salainen");
+    (1, 'Admin', '635fb41d21af00dc4a9ef7401b729c30f33b67261e3189346dd2507eb2e2d124b42a85852133c859', 1);
     
+INSERT INTO
+	authority
+VALUES
+	(2,'ROLE_ADMIN'),
+	(1,'ROLE_USER');
+	
+	INSERT INTO
+	webuser2_authority
+VALUES
+	(1,1,2);
